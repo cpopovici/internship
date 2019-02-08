574e7bdf146f9dc91755f2ec145225840119593e
Cristian Popovici <kristianrz16@hotmail.com>
2019-02-08 11:57:37 +0200

    uppload oop uml diagram

---
+++ C#/01. VCS/GitAppDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine("fooo");
+            Console.WriteLine("fooo2");
+        }
+    }
+}
---
+++ C#/01. VCS/GitAppDemo/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitAppDemo
+{
+    class Users
+    {
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public int UserPhone { get; set; }
+        public string UserImage_1 { get; set; }
+        public string UserImage_2 { get; set; }
+    }
+}
