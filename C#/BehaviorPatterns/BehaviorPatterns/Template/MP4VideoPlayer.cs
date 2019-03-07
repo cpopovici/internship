25583e600a4c250150a6444f8add0aa74994dd82
Cristian Popovici <kristianrz16@hotmail.com>
2019-03-07 18:29:55 +0200

    added completed tasks

---
+++ C#/BehaviorPatterns/BehaviorPatterns/Template/MP4VideoPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviorPatterns.Template
+{
+    class MP4VideoPlayer : VideoPlayer
+    {
+        public override void DecodeVideo()
+        {
+            Console.WriteLine("  Decoding MP4 video file ...");
+        }
+    }
+}
---
+++ C#/BehaviorPatterns/BehaviorPatterns/Template/VideoPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviorPatterns.Template
+{
+    public abstract class VideoPlayer
+    {
+        public void LoadFile(string fileName)
+        {
+            Console.WriteLine($"Loading {fileName} video ...");
+            Console.WriteLine($"  {fileName} Loaded!");
+        }
+
+        public abstract void DecodeVideo();
+
+        public void StartPlay()
+        {
+            Console.WriteLine($"  Video File starts playing!\n");
+        }
+
+        public void PlayVideo(string fileName)
+        {
+            LoadFile(fileName);
+            DecodeVideo();
+            StartPlay();
+        }
+    }
+}
