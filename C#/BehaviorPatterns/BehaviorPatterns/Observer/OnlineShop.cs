6be0daf1920152b426c850c951f4f6bcfcde508a
Cristian Popovici <kristianrz16@hotmail.com>
2019-03-07 17:11:22 +0200

    aded behavior patterns

---
+++ C#/BehaviorPatterns/BehaviorPatterns/Observer/OnlineShop.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviorPatterns.Observer
+{
+    class OnlineShop
+    {
+        private List<ICustomerObserver> _observers;
+        private List<Product> products;
+       
+        public OnlineShop()
+        {
+            _observers = new List<ICustomerObserver>();
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+            Notify();
+        }
+
+        public void Subscribe(ICustomerObserver customer)
+        {
+            _observers.Add(customer);
+        }
+
+        public void Unsubscripe(ICustomerObserver customer)
+        {
+            _observers.Remove(customer);
+        }
+
+        private void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update($"To Online Shop added new item:\n  {products.Last()}");
+            }
+        }
+
+  
+    }
+}
---
+++ C#/BehaviorPatterns/BehaviorPatterns/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BehaviorPatterns.Observer;
+using BehaviorPatterns.Template;
+
+namespace BehaviorPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Observer Example
+            var customer = new Customer("Eliot Anderson");
+            var customer2 = new Customer("Andrei Keks");
+            var manager = new Manager("Rami Malek");
+            var shop = new OnlineShop();
+            var mobile = new Product("Xiaomi MI 9", 600);
+
+            shop.Subscribe(customer);
+            shop.Subscribe(customer2);
+            shop.AddProduct(mobile);
+
+            // Template pattern example
+            var mkvVideo = new MkvVideoPlayer();
+            var mp4Video = new MP4VideoPlayer();
+
+            mkvVideo.PlayVideo("funny-cats.mkv");
+            mp4Video.PlayVideo("funny-dogs.mp4");
+            Console.ReadLine();
+
+        }
+    }
+}
